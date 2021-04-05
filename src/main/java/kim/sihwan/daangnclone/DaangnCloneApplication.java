69254ba568550a7f6664659f2b91d763ca884411
Kim-SiHwan <tjdans4166@naver.com>
2021-04-05 14:30:55 +0900

    프로젝트 생성

---
+++ src/main/java/kim/sihwan/daangnclone/DaangnCloneApplication.java
@@ -0,0 +1,13 @@
+package kim.sihwan.daangnclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaangnCloneApplication.class, args);
+    }
+
+}

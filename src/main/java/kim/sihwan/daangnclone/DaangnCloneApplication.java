097f2e066d95dbdb07a04c40dfde9da0efd7d446
Kim-SiHwan <tjdans4166@naver.com>
2021-04-14 16:38:56 +0900

    redis 설정

--- src/main/java/kim/sihwan/daangnclone/DaangnCloneApplication.java
+++ src/main/java/kim/sihwan/daangnclone/DaangnCloneApplication.java
@@ -2,10 +2,11 @@ package kim.sihwan.daangnclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class DaangnCloneApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DaangnCloneApplication.class, args);
     }

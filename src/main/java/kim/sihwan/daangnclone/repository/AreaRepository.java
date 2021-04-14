b473ca5b846f68865209e726ba1ba0e31b40e840
Kim-SiHwan <tjdans4166@naver.com>
2021-04-14 16:39:07 +0900

    area Repo

---
+++ src/main/java/kim/sihwan/daangnclone/repository/AreaRepository.java
@@ -0,0 +1,12 @@
+package kim.sihwan.daangnclone.repository;
+
+import kim.sihwan.daangnclone.domain.Area;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AreaRepository extends JpaRepository<Area,Long> {
+
+    List<Area> findAllByCityLike(String city);
+
+}

8006665e6bb2377f777d3e7c2b2cf65c11e7b9af
Kim-SiHwan <tjdans4166@naver.com>
2021-05-01 17:08:19 +0900

    Security Config

--- src/main/java/kim/sihwan/daangnclone/config/security/SecurityConfig.java
+++ src/main/java/kim/sihwan/daangnclone/config/security/SecurityConfig.java
@@ -1,17 +1,24 @@
 package kim.sihwan.daangnclone.config.security;
 
 import kim.sihwan.daangnclone.config.cors.SimpleCorsFilter;
+import kim.sihwan.daangnclone.config.jwt.JwtAccessDeniedHandler;
+import kim.sihwan.daangnclone.config.jwt.JwtAuthenticationEntryPoint;
+import kim.sihwan.daangnclone.config.jwt.JwtSecurityConfig;
+import kim.sihwan.daangnclone.config.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -19,6 +26,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final SimpleCorsFilter corsFilter;
+    private final JwtTokenProvider tokenProvider;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Override
     public void configure(WebSecurity web) {
@@ -37,21 +47,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
                 .csrf().disable()
                 .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
+
                 // enable h2-console
                 .headers()
                 .frameOptions()
                 .sameOrigin()
 
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
                 .and()
                 .authorizeRequests()
                 .antMatchers("/h2/**").permitAll()
+                .antMatchers("/api/member/test").permitAll()
+                .antMatchers("/api/member/join").permitAll()
+                .antMatchers("/api/member/login").permitAll()
+                .antMatchers("/api/product/**").permitAll()
                 .antMatchers("/test/v1").permitAll()
-                .antMatchers("/testapi/test/vv").permitAll()
-                .anyRequest().authenticated();
-
+                .antMatchers("/testapi/test/vv").permitAll() .anyRequest().authenticated()
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
 
 
     }
